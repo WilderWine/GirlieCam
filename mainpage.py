01eaae420d765a6b7bf6e9b8841bf29df1a40d1e
wilderwine <vernikon72@gmail.com>
2024-12-08 21:04:40 +0300

    add connection between app and firebase realtime db

--- mainpage.py
+++ mainpage.py
@@ -1,3 +1,18 @@
 import dlib
 import tkinter as tk
 import cv2
+import firebase_admin
+import requests
+from firebase_admin import credentials, db
+
+
+cred = credentials.Certificate('girliecam-firebase-adminsdk-no9ee-a4c71905f6.json')
+firebase_admin.initialize_app(cred, {"databaseURL": "https://girliecam-default-rtdb.firebaseio.com/"})
+
+ref = db.reference("/")
+print(ref.get())
+db.reference("/item").set({})
+db.reference("/item/item1").set([1,3,3])
+print(ref.get())
+db.reference("/item").delete()
+print(ref.get())
